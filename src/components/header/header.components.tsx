b4bf55a2ab1485999e19dba1dee0f888287b1619
Kleyton Ferreira <kleytondev40@gmail.com>
2026-01-16 12:09:52 -0300

    feat: add styled Components to Header Componets

--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -1,21 +1,25 @@
 import { BsCart3 } from 'react-icons/bs'
-
-import './header.styles.css'
+import {
+  HeaderContainer,
+  HeaderItem,
+  HeaderItems,
+  HeaderTitle
+} from './header.styles'
 
 const Header = () => {
   return (
-    <div className='header-container'>
-      <h2 className='header-title'>CLUB CLOTHING</h2>
-      <div className='header-items'>
-        <div className='header-item'>Explorar</div>
-        <div className='header-item'>Login</div>
-        <div className='header-item'>Criar Conta</div>
-        <div className='header-item'>
+    <HeaderContainer>
+      <HeaderTitle>CLUB CLOTHING</HeaderTitle>
+      <HeaderItems>
+        <HeaderItem>Explorar</HeaderItem>
+        <HeaderItem>Login</HeaderItem>
+        <HeaderItem>Criar Conta</HeaderItem>
+        <HeaderItem>
           <BsCart3 size={20} />
           <p style={{ marginLeft: 5 }}>5</p>
-        </div>
-      </div>
-    </div>
+        </HeaderItem>
+      </HeaderItems>
+    </HeaderContainer>
   )
 }
 
