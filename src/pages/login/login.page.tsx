5bc900ab1affdf2a38a07d7c9fe78a73ebb41978
Kleyton Ferreira <kleytondev40@gmail.com>
2026-01-16 21:15:58 -0300

    feat: add Login Page email and password labels

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -27,9 +27,11 @@ const LoginPage = () => {
           </CustomButton>
           <LoginSubtitle>ou entre com seu e-mail </LoginSubtitle>
           <LoginInputContainer>
+            <p>E-mail</p>
             <CustomInput placeholder='Digite seu e-mail' />
           </LoginInputContainer>
           <LoginInputContainer>
+            <p>Senha</p>
             <CustomInput placeholder='Digite seu Senha' />
           </LoginInputContainer>
           <CustomButton startIcon={<FiLogIn size={18} />}>Entar</CustomButton>
